8e1b26e6dd8c04c1fe74ff8d13639c2748c1c4b9
LuBeDa <lb@sell-e.de>
2019-02-10 12:16:33 +0100

    First working version

--- awtrix-server/run.sh
+++ awtrix-server/run.sh
@@ -1,3 +1,19 @@
-cp /awtrix.jar /data/awtrix.jar
-cd /data
+#!/usr/bin/with-contenv bash
+
+folder="/data/config"
+if [ -d "$file" ]
+then
+	echo "$file found."
+else
+    echo "$file not found."
+    echo "mkdir"
+    mkdir /data/config
+    mkdir /data/plugins
+    echo "symlink"
+    ln -s /data/config /config
+    ln -s /data/plugins /plugins
+    cp /datastore /data/config
+fi
+echo "Starting"
 java -jar /awtrix.jar
+
